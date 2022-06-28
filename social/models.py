0e714ee74233c9942e7dce1a4ff6c4b35ecdfc69
jessicarydberg <jessica.rydberg@hotmail.se>
2022-06-28 10:19:20 +0000

    Create event model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
+    date = models.DateTimeField('Event Date')
+    content = models.TextField()
+    location = models.CharField(max_length=120)
+    attendees = models.CharField(User, related_name='event_attendees', blank=True)
+    image = models.CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_attendees(self):
+        return self.attendees.count()
