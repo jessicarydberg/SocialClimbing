13a47c7166388e84a22390c61c905d22c0509376
jessicarydberg <jessica.rydberg@hotmail.se>
2022-06-28 10:25:10 +0000

    Add Comment model

--- social/models.py
+++ social/models.py
@@ -25,3 +25,17 @@ class Event(models.Model):
 
     def number_of_attendees(self):
         return self.attendees.count()
+
+
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(dafult=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
