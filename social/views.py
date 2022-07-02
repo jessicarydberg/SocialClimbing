8332b9644f1cac82d58f20d38770400e30cf4859
jessicarydberg <jessica.rydberg@hotmail.se>
2022-07-02 09:30:00 +0000

    Create view code

--- social/views.py
+++ social/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.db import models
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+
+class EventList(generic.ListView):
+    today = models.DateTimeField(auto_now=True)
+    if Event.date > today:
+        model = Event
+        queryset = Event.objects.filter(status=1).order_by('date')
+        template_name = 'index.html'
+        paginate_by = 6
+
