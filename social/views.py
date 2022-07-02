fbda90d8fe8657a3ff3434bda4a200d64521b835
jessicarydberg <jessica.rydberg@hotmail.se>
2022-07-02 20:06:35 +0000

    Add sign in, up and out function for users

--- social/views.py
+++ social/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 
@@ -8,3 +8,24 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by("date")
     template_name = "index.html"
     paginate_by = 8
+
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        comments = event.comments.filter(approved=True).order_by('created_on')
+        attended = False
+        if event.attendees.filter(id=self.request.user.id).exists():
+            attended = True
+
+        return render(
+            request,
+            "event.html",
+            {
+                "event": event,
+                "comments": comments,
+                "attended": attended
+            },
+        )
